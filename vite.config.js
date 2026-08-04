818a8e0332c591ad3bc1eea3683959244b2d1bee
Evgenii Rubin <kubikus2009@yandex.ru>
2026-08-04 18:47:22 +0200

    chore: remove completed test and QA environment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.js",
-    css: true,
-    exclude: ["e2e/**", "node_modules/**", "dist/**"],
-  },
 });
